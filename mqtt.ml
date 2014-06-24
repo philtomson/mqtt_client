537686b4c4967404c9785786b893ccfd0ae9380a
PhilTomson <philtomson@gmail.com>
2014-06-24 13:29:26 -0700

    added int_to_msg_type

--- mqtt.ml
+++ mqtt.ml
@@ -29,6 +29,25 @@ let msg_type_to_int msg = match msg with
 | DISONNECT   -> 14
 | RESERVED    -> 15
 
+let int_to_msg_type b = match b with
+  1 -> Some CONNECT 
+| 2 -> Some CONNACK     
+| 3 -> Some PUBLISH     
+| 4 -> Some PUBACK      
+| 5 -> Some PUBREC 
+| 6 -> Some PUBREL
+| 7 -> Some PUBCOMP    
+| 8 -> Some SUBSCRIBE  
+| 9 -> Some SUBACK      
+| 10 -> Some UNSUBSCRIBE 
+| 11 -> Some UNSUBACK   
+| 12 -> Some PINGREQ    
+| 13 -> Some PINGRESP    
+| 14 -> Some DISONNECT   
+| 15 -> Some RESERVED    
+| _  -> None
+
+
 let max_packet_size = 128 
 let keepalive       = 15
 let version         = 3
@@ -45,6 +64,7 @@ let do_while f p ~init =
   in
   loop init
 
+
 (*do_while (fun v ->
             let v = succ v in
             Printf.printf "%d\n" v;
@@ -52,18 +72,36 @@ let do_while f p ~init =
          (fun v -> v mod 6 <> 0)
          ~init:0 *)
 
-type message = {   msg:        msg_type;
-                   dup:        bool;
-                   qos:        int;
-                   retain:     bool;
-                   payload:    string 
-               }
+(*
+let get_remaining_len instream = 
+  let multiplier = 1 in
+  let value = 0 
+  do_while (fun _ ->
+              let buffer_size = 1 in 
+              let buffer = String.create buffer_size in 
+              let bytes_back = read instream buffer 0 1 in (* get the next byte *)
+              let digit = Char.code buffer.[0]
+              value += (digit land 127) * multiplier 
+              multiplier *= 128; digit) 
+            (fun digit -> ((digit AND 128) != 0))
+*)
 
-let msg_header msg dup qos retain =
-  (((msg_type_to_int msg) land 0xFF) lsl 4) lor
-  ((if dup then 0x1 else 0x0) lsl        3) lor
-  ((qos land 0x03) lsl                   1) lor
-  (if retain then 0x1 else 0x0)
+let get_remaining_len instream = 
+  let rec aux multiplier value = 
+    let buffer = String.create 1 in 
+    let bytes_back = read instream buffer 0 1 in (* get the next byte *)
+    let digit = Char.code buffer.[0] in
+    match ((digit land 127)  ) with
+      0 -> value
+    | _ -> aux (multiplier * 128) ( value + ((digit land 127) * multiplier)) in
+  aux 1 0
+
+type header = {    msg:           msg_type;
+                   dup:           bool;
+                   qos:           int;
+                   retain:        bool;
+                   remaining_len: int
+               }
 
 let rec str_to_charlist s = 
   let rec aux s lst = 
@@ -72,7 +110,33 @@ let rec str_to_charlist s =
     0 -> lst
   | n -> (aux (String.sub s 1 (len-1)) (s.[0]::lst)) in
   List.rev (aux s []) 
+
+let str_to_intlist s = List.map (fun c -> Char.code c) (str_to_charlist s)
   
+let msg_header msg dup qos retain =
+  (((msg_type_to_int msg) land 0xFF) lsl 4) lor
+  ((if dup then 0x1 else 0x0) lsl        3) lor
+  ((qos land 0x03) lsl                   1) lor
+  (if retain then 0x1 else 0x0)
+
+let deconstruct_header bytes = 
+  let byte_1 = Char.code bytes.[0] in
+  let byte_2 = Char.code bytes.[1] in
+  let mtype = match (int_to_msg_type ((byte_1 lsr 4) land 0xFF)) with
+    | None -> failwith "Not a legal msg type!"
+    | Some msg -> msg
+  in
+  let dup   = if ((byte_1 lsr 3) land 0x01) = 1 then true else false in
+  let qos   = (byte_1 lsr 1) land 0x3 in
+  let retain= if (byte_1 land 0x01) = 1 then true else false in
+  { msg= mtype; dup= dup; qos= qos; retain= retain; 
+    remaining_len= byte_2 }
+
+(*
+let get_header = 
+*)
+
+
 
 let charlist_to_str l =
   let res = String.create (List.length l) in
@@ -86,6 +150,8 @@ let packet = List.map (fun b -> Char.chr b) [0x10;0x23;0x0 ; 0x6; 0x4d; 0x51; 0x
 (*
 
 *)
+
+
 
 let receive_connack sock = 
   let buffer_size = 4096 in 
@@ -102,10 +168,10 @@ let receive_connack sock =
 let connect_to_broker server_name port_num f =
   let connect_str = charlist_to_str [
     char_of_int (msg_header CONNECT false 0 false); 
-    Char.chr 19; (* should be 12 ?*)
-    Char.chr 0x00; 
-    Char.chr 0x06; 
-    'M';'Q';'I';'s';'d';'p'; 
+    Char.chr 19; (* remaining length *)
+    Char.chr 0x00; (* protocol length MSB *) 
+    Char.chr 0x06; (* protocol length LSB *) 
+    'M';'Q';'I';'s';'d';'p'; (* protocol *)
     Char.chr version; 
     Char.chr 0x00; (* connect flags  why was it CE?*)
     Char.chr 0x00; (* keep alive timer MSB*)
